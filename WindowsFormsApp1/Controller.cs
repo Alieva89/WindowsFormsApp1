21e723d4fa6e48408a0eaf6cdc6c775cd0e6beac
user <user@C1010>
2022-03-22 12:15:13 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Controller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Controller
+    {
+        private Model _model;
+
+        private enum Action
+        {
+            SUM;
+            SUB;
+            DEV;
+            MULTY;
+        }
+
+        public Controller(Model model)
+        {
+            _model = model;
+        }
+        /* проверка model */
+
+    }
+
+    internal void CalculateResult(Action action a, int b)
+    {
+        switch (action)
+        {
+            case Action.SUM:
+                _model.SUM(a,b);
+                break;
+            case Action.SUM:
+                _model.SUB(a, b);
+                break;
+            case Action.SUM:
+                _model.DEV(a, b);
+                break;
+            case Action.SUM:
+                _model.MULTY(a, b);
+                break;
+
+        }
+    }
+    {
+        _model.Sum(a, b);
+    }
+}
---
+++ WindowsFormsApp1/model.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class model: Controller
+
+    {
+
+        public delegate void ResultDelegate(int result);
+        public event ResultDelegate NotiFicate;
+        public int Sum(int a, int b)
+        {
+            NotiFicate.Invoke(a + b);
+        }
+
+        public int Sub(int a, int b)
+        {
+            NotiFicate.Invoke(a - b);
+        }
+        public int Dev(int a, int b)
+        {
+            NotiFicate.Invoke(a * b);
+        }
+        public int Multy(int a, int b)
+        {
+            NotiFicate.Invoke(a / b);
+        }
+    }
+}
